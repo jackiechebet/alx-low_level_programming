8d073746043a9ef9c2ebbc09f0f90beff750319d
jackiechebet <jackieblet@gmail.com>
2023-02-19 19:44:37 -0800

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - print sizes of int, long int,long long int,
+ * char and float in bytes
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of a char: %zu byte\n", sizeof(charType));
+	printf("Size of a int: %zu byte\n", sizeof(intType));
+	printf("Size of a long int: %zu byte\n", sizeof(intType));
+	printf("Size of a long long int: %zu byte\n", sizeof(intType));
+	printf("Size of a float: %zu bytes\n", sizeof(floatType));
+
+	return (0);
+}
