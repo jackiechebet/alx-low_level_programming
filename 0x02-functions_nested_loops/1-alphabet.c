6b19c9290e4aa2a8747dfcc3f5c2a17c1bf4a00d
jackiechebet <jackieblet@gmail.com>
2023-02-25 17:21:17 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,6 @@
 
 /**
  * print_alphabet - program that prints letters of alphabets
- * Return: always 0
  */
 
 void print_alphabet(void)
@@ -13,5 +12,4 @@ void print_alphabet(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
-	return (0);
 }
