69bb459623e397efb8a3e716a79be00258a88ba0
jackiechebet <jackieblet@gmail.com>
2023-03-02 07:57:59 -0800

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - function that draws a straight line
+ * @n: integer input
+ * Return: '_' when n>0 and 0 when n==0
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+
+	}
+}
