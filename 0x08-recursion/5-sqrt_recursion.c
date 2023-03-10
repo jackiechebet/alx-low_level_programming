ea377319a7acc820a75b8b5e8ca3574070a17a78
jackiechebet <jackieblet@gmail.com>
2023-03-09 18:08:30 -0800

    6-prime

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *  _sqrt_recursion - returns natural square root of a number
+ *  @n: input
+ *  Return: natural square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+	return (0);
+}
