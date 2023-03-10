7844013974473a552c22a1cf109f6b49669f5fac
jackiechebet <jackieblet@gmail.com>
2023-03-09 18:39:31 -0800

    palindrome

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i)
+
 /**
  *  _sqrt_recursion - returns natural square root of a number
  *  @n: input
@@ -6,5 +10,24 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (0);
+	return (_sqrt(n, 1));
+}
+/**
+  * _sqrt - Calculates natural square root
+  * @n: number to calculate the square root
+  * @i: iterate number
+  *
+  * Return: the natural square root
+  */
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+
+	if (sqrt == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
 }
